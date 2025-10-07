3128466172cec50eabcf5a5febec0a6fb46c9b53
dany-bg <dany2006forever@gmail.com>
2025-10-08 01:35:18 +0200

    fix: fix game panel size

--- app/src/main/java/game/ui/GamePanel.java
+++ app/src/main/java/game/ui/GamePanel.java
@@ -19,13 +19,13 @@ public class GamePanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
-        int size = Math.min(getWidth() - 4, getHeight() - 4) / 10;
+        int size = Math.min(getWidth() - 4, getHeight() - 4) / 20;
         int width = getWidth() - (size * 2);
         int height = getHeight() - (size * 2);
 
-        int y = (getHeight() - (size * 10)) / 2;
-        for (int horz = 0; horz < 10; horz++) {
-            int x = (getWidth() - (size * 10)) / 2;
+        int y = (getHeight() - (size * 20)) / 2;
+        for (int horz = 0; horz < 20; horz++) {
+            int x = (getWidth() - (size * 20)) / 2;
             for (int vert = 0; vert < 10; vert++) {
                 g.drawRect(x, y, size, size);
                 x += size;
