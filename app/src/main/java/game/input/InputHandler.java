5c427a6e538dee1cbc730efdd7c2de75699e065d
dany-devBg <danielyasenov.student@gmail.com>
2025-10-10 22:27:01 +0200

    SCRUM-19: Create piece moving logic and keybinds (#6)

---
+++ app/src/main/java/game/input/InputHandler.java
@@ -0,0 +1,35 @@
+package game.input;
+
+import game.ui.TetrisPiece;
+import java.awt.event.ActionEvent;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+
+public class InputHandler {
+
+    private JComponent panel;
+
+    public InputHandler(JComponent panel) {
+        this.panel = panel;
+    }
+
+    public void bindKey(String keyStroke, String actionName, Supplier<TetrisPiece> pieceSupplier,
+            Consumer<TetrisPiece> action) {
+        panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+                .put(KeyStroke.getKeyStroke(keyStroke), actionName);
+        panel.getActionMap().put(actionName, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TetrisPiece tetrisPiece = pieceSupplier.get();
+                if (tetrisPiece != null) {
+                    action.accept(tetrisPiece);
+                    panel.repaint();
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/game/ui/GamePanel.java
+++ app/src/main/java/game/ui/GamePanel.java
@@ -1,16 +1,15 @@
 package game.ui;
 
+import game.input.InputHandler;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements KeyListener, ActionListener {
+public class GamePanel extends JPanel implements ActionListener {
     public static final int ROWS = 20;
     public static final int COLS = 10;
     public static final int BOX_SIZE = 45;
@@ -19,18 +18,27 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 
     private Timer timer;
     private TetrisPiece currentPiece;
+    private InputHandler inputHandler;
 
     public GamePanel() {
 
         this.topLeftX = (GameFrame.WINDOW_WIDTH - BOX_SIZE * COLS) / 2;
         this.topLeftY = (GameFrame.WINDOW_HEIGHT - BOX_SIZE * ROWS) / 2;
-        System.out.println(topLeftX);
 
         currentPiece = TetrisPiece.randomPiece();
         timer = new Timer(1000, this);
         timer.start();
 
-        addKeyListener(this);
+        inputHandler = new InputHandler(this);
+
+        inputHandler.bindKey("RIGHT", "moveRightAction", () -> this.getCurrentPiece(), TetrisPiece::moveRight);
+        inputHandler.bindKey("D", "moveRightAction", () -> this.getCurrentPiece(), TetrisPiece::moveRight);
+
+        inputHandler.bindKey("LEFT", "moveLeftAction", () -> this.getCurrentPiece(), TetrisPiece::moveLeft);
+        inputHandler.bindKey("A", "moveLeftAction", () -> this.getCurrentPiece(), TetrisPiece::moveLeft);
+
+        inputHandler.bindKey("DOWN", "moveDownAction", () -> this.getCurrentPiece(), TetrisPiece::moveDown);
+        inputHandler.bindKey("S", "moveDownAction", () -> this.getCurrentPiece(), TetrisPiece::moveDown);
     }
 
     @Override
@@ -81,15 +89,7 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 
     }
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
+    public TetrisPiece getCurrentPiece() {
+        return this.currentPiece;
     }
 }
